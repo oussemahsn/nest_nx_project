95c2273ffbfe1f435657d9fc4121d8efa78ea16a
oussemahsn <oussema.hasnaoui@devoteam.com>
2022-02-08 13:37:02 +0100

    final commit

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CoreModule } from "@beehive/core";
+
+
+@Module({
+  imports: [CoreModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
